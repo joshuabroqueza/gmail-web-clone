d99426c5b51681a4280060b5bf9943c7aaa5e84a
Joshua Broqueza <joshuabroqueza@yahoo.com>
2022-08-02 02:46:23 +0800

    Complete
    With google authentication login
    
    https://linkedin-clone-yt-19e25.web.app

--- src/Header.js
+++ src/Header.js
@@ -1,38 +1,48 @@
-import React from "react";
-import "./Header.css";
-import MenuIcon from "@mui/icons-material/Menu";
-import { Avatar, IconButton } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import AppsIcon from "@mui/icons-material/Apps";
-import NotificationsIcon from "@mui/icons-material/Notifications";
+import React from 'react';
+import './Header.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import { autocompleteClasses, Avatar, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { selectUser, logout } from './features/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { auth } from './firebase';
 
 function Header() {
-  Number();
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  const signOut = () => {
+    auth.signOut().then(() => {
+      dispatch(logout());
+    });
+  };
   return (
-    <div className="header">
-      <div className="header__left">
+    <div className='header'>
+      <div className='header__left'>
         <IconButton>
           <MenuIcon />
         </IconButton>
         <img
-          src="https://images.macrumors.com/t/QY0KdwbObUzURWrw5C2buSSZseY=/400x0/article-new/2020/10/newgmaillogo.0.jpg?lossy"
-          alt=""
+          src='https://images.macrumors.com/t/QY0KdwbObUzURWrw5C2buSSZseY=/400x0/article-new/2020/10/newgmaillogo.0.jpg?lossy'
+          alt=''
         />
       </div>
-      <div className="header__middle">
+      <div className='header__middle'>
         <SearchIcon />
-        <input type="text" placeholder="Search mail" />
-        <ArrowDropDownIcon className="header__inputCaret" />
+        <input type='text' placeholder='Search mail' />
+        <ArrowDropDownIcon className='header__inputCaret' />
       </div>
-      <div className="header__right">
+      <div className='header__right'>
         <IconButton>
           <AppsIcon />
         </IconButton>
         <IconButton>
           <NotificationsIcon />
         </IconButton>
-        <Avatar />
+        <Avatar src={user.photoUrl} onClick={signOut} />
       </div>
     </div>
   );
